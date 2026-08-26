84374bc29ef54c6f2e9e3c74f42d96724680e5cb
HUB_Optimus <voxterraeoptimus@proton.me>
2026-08-26 17:36:55 +0200

    fix(operator): bind canary entrypoint to eu-west-1
    Keep the canary account and region fixed at the reviewed values.

--- ops/aws/operator-url-intake/bin/hub-optimus-operator-infra.ts
+++ ops/aws/operator-url-intake/bin/hub-optimus-operator-infra.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib/core';
 import { HubOptimusOperatorInfraStack } from '../lib/hub-optimus-operator-infra-stack';
 import { Config, resolveAwsRegion } from '../lib/config';
 
-const app = new cdk.App();
+const app = new cdk.App({ analyticsReporting: false });
 const region = resolveAwsRegion(app.node.tryGetContext('awsRegion'));
 new HubOptimusOperatorInfraStack(app, 'hub-optimus-operator-infra', {
   stackName: 'hub-optimus-operator-infra',
